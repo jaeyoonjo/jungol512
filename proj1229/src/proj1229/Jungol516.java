6caeb67dbd69c086492686a008f96aee1f6517a0
재윤 <@DESKTOP-T7JVA8D>
2021-12-29 11:55:12 +0900

    second commit

---
+++ proj1229/src/proj1229/Jungol516.java
@@ -0,0 +1,21 @@
+package proj1229;
+
+import java.util.Scanner;
+
+public class Jungol516 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Scanner sc = new Scanner(System.in);
+          double d1 = sc.nextDouble();
+          double d2 = sc.nextDouble();
+          String c = sc.next();
+          sc.close();
+          
+          System.out.printf("%.2f\n", d1);
+          System.out.printf("%.2f\n", d2);
+          System.out.printf(c);
+          
+	}
+
+}
